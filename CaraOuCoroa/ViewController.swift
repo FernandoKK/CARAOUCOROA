079778d0fded27c6ccffd498e37e2e3c3c98e8d7
Fernando K <fernandokk@gmail.com>
2020-05-03 16:31:39 -0300

    Initial Commit

---
+++ CaraOuCoroa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaraOuCoroa
+//
+//  Created by Fernando Cesar Kovaltchuk on 03/05/20.
+//  Copyright © 2020 Fernando Cesar Kovaltchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
